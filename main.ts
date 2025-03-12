66024031b498a2e252a0231d11398f85c67b6d82
APOLLLO23 <00001144351789sp@al.educacao.sp.gov.br>
2025-03-12 13:33:31 -0300

    Atualizar 'pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+let direçao = 0
 basic.forever(function () {
-	
+    direçao = input.compassHeading()
+    if (direçao < 45 || direçao > 315) {
+        basic.showString("n")
+    } else {
+        basic.showString("n")
+    }
 })
